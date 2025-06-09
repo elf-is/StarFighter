82d845712dfbad7c3b65f49757237b476e66e2df
Ismail El Fekak <ismail.elfekak@gmail.com>
2025-06-09 20:08:28 +0100

    Implement BuildStarFighters method

--- StarFighterLib/StarFighterService.cs
+++ StarFighterLib/StarFighterService.cs
@@ -1,16 +1,33 @@
 ﻿namespace StarFighterLib;
 
-
 public class StarFighterService : IStarFighterService
 {
     public IList<StarFighter> BuildStarFighters(string parts)
     {
-        throw new NotImplementedException();
+        IList<StarFighter> starFighters = new List<StarFighter>();
+        List<char> partsList = parts.ToList();
+        bool hasFighter = true;
+        while (hasFighter)
+        {
+            if (HasFirstFighter(partsList))
+            {
+                starFighters.Add(new StarFighter());
+                continue;
+            }
+
+            hasFighter = false;
+        }
+
+        return starFighters;
     }
 
     public IList<StarFighter> BuildBestStarFighters(string parts)
-    {
-        throw new NotImplementedException();
-    }
+        => throw new NotImplementedException();
 
+    private static bool HasFirstFighter(List<char> partsList)
+        => partsList.Remove('(') &&
+           partsList.Remove('-') &&
+           partsList.Remove('O') &&
+           partsList.Remove('-') &&
+           partsList.Remove(')');
 }
