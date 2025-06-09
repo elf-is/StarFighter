50c7044f2470ec90c4da9878e6493c18b7deb7dd
Ismail El Fekak <ismail.elfekak@gmail.com>
2025-06-09 23:56:18 +0100

    Implement BuildBestStarFighters method

--- StarFighterLib/StarFighterService.cs
+++ StarFighterLib/StarFighterService.cs
@@ -22,7 +22,47 @@ public class StarFighterService : IStarFighterService
     }
 
     public IList<StarFighter> BuildBestStarFighters(string parts)
-        => throw new NotImplementedException();
+    {
+        Dictionary<char, int> partCount = new()
+        {
+            { '(', 0 },
+            { ')', 0 },
+            { '-', 0 },
+            { 'O', 0 },
+            { '[', 0 },
+            { ']', 0 },
+        };
+
+        foreach (char part in parts)
+        {
+            partCount[part]++;
+        }
+
+        int maxType2 = Math.Min(Math.Min(partCount['['], partCount[']']), Math.Min(partCount['-'] / 4, partCount['O']));
+
+        List<StarFighter> bestStarFighters = new();
+        for (int i = 0; i < maxType2; i++)
+        {
+            partCount['[']--;
+            partCount[']']--;
+            partCount['-'] -= 4;
+            partCount['O']--;
+
+            bestStarFighters.Add(new StarFighter(20));
+
+            int maxType1 = Math.Min(Math.Min(partCount['('], partCount[')']), Math.Min(partCount['-'] / 2, partCount['O']));
+            for (int j = 0; j < maxType1; j++)
+            {
+                partCount['(']--;
+                partCount[')']--;
+                partCount['-'] -= 2;
+                partCount['O']--;
+                bestStarFighters.Add(new StarFighter(15));
+            }
+        }
+
+        return bestStarFighters;
+    }
 
     private static bool HasFirstFighter(List<char> partsList)
         => partsList.Remove('(') &&
